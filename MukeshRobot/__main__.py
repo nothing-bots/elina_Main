e98eafecfbd2e54ddda40f5d202ebcfbaa781562
Tosu <terebina860@gmail.com>
2024-12-03 05:00:48 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -271,7 +271,7 @@ def start(update: Update, context: CallbackContext):
             first_name = update.effective_user.first_name
             
             x=update.effective_message.reply_sticker(
-                "CAACAgUAAxUAAWdOQi9HnonpKtHDoxH88MqkRw3kAAIsFAACduFwVoIHneWcUj5CNgQ)
+                "CAACAgUAAxUAAWdOQi9HnonpKtHDoxH88MqkRw3kAAIsFAACduFwVoIHneWcUj5CNgQ")
             x.delete()
             usr = update.effective_user
             lol = update.effective_message.reply_text(
