12475bdf1c779d9f8c455a81ba5482b24e456dbc
Tosu <terebina860@gmail.com>
2024-12-03 05:12:37 +0530

    Update good_evening.py

--- MukeshRobot/modules/good_evening.py
+++ MukeshRobot/modules/good_evening.py
@@ -33,10 +33,10 @@ PHOTO = "https://graph.org/file/4126cda3b1ff1c43f70aa.mp4"
 
 @register(pattern=("Good evening"))
 async def awake(event):
-    NEKO = f"❀ ᴡɪsʜɪɴɢ ʏᴏᴜ ᴀ ᴠᴇʀʏ ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ ❀\n\n✦ ᴇᴠᴇɴɪɴɢs ᴀʀᴇ ᴛʜᴇ ʙᴇᴀᴜᴛɪғᴜʟʟʏ sᴡᴇᴇᴛ sᴘᴏᴛ ʙᴇᴛᴡᴇᴇɴ ᴛʜᴇ ʜᴀʀsʜ ʟɪɢʜᴛ ᴏғ ᴛʜᴇ ᴅᴀʏ ᴀɴᴅ ᴛʜᴇ ᴅᴇᴀᴅ ᴅᴀʀᴋɴᴇss ᴏғ ɴɪɢʜᴛ.\n\n✦ ᴡɪsʜɪɴɢ ᴛᴏ ➛ {event.sender.first_name}\n\n✦ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➛ [ᴢᴇʀᴏ ᴛᴡᴏ](https://t.me/zerotwo_xprobot)"
+    NEKO = f"❀ ᴡɪsʜɪɴɢ ʏᴏᴜ ᴀ ᴠᴇʀʏ ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ ❀\n\n✦ ᴇᴠᴇɴɪɴɢs ᴀʀᴇ ᴛʜᴇ ʙᴇᴀᴜᴛɪғᴜʟʟʏ sᴡᴇᴇᴛ sᴘᴏᴛ ʙᴇᴛᴡᴇᴇɴ ᴛʜᴇ ʜᴀʀsʜ ʟɪɢʜᴛ ᴏғ ᴛʜᴇ ᴅᴀʏ ᴀɴᴅ ᴛʜᴇ ᴅᴇᴀᴅ ᴅᴀʀᴋɴᴇss ᴏғ ɴɪɢʜᴛ.\n\n✦ ᴡɪsʜɪɴɢ ᴛᴏ ➛ {event.sender.first_name}\n\n✦ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➛ [『 ＥＬＩＮＡ 』](https://t.me/Elina_Roxbot)"
     BUTTON = [
         [
-            Button.url("ᴍᴇᴇᴛ ᴍᴇ ʜᴇʀᴇ ʙᴀʙʏ", "https://telegram.dog/dragons_society"),
+            Button.url("ᴍᴇᴇᴛ ᴍᴇ ʜᴇʀᴇ ʙᴀʙʏ", "https://telegram.me/anime_x_group"),
         ]
     ]
     await telethn.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
